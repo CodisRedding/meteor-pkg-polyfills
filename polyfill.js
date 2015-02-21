0a08f0b9835579d8892480ec5e97d197f06bb03b
Rocky Assad <rassad@gbhem.org>
2015-02-21 12:37:19 -0600

    add polyfill for bind()

--- polyfill.js
+++ polyfill.js
@@ -1,8 +1,36 @@
-// polyfill dat create
+// Object.create
 if (!Object.create) {
   Object.create = function (o) {
     function F() {}
     F.prototype = o;
     return new F();
+  };
+}
+
+
+// Function.prototype.bind
+// from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/function/bind
+if (!Function.prototype.bind) {
+  Function.prototype.bind = function(oThis) {
+    if (typeof this !== 'function') {
+      // closest thing possible to the ECMAScript 5
+      // internal IsCallable function
+      throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+    }
+
+    var aArgs   = Array.prototype.slice.call(arguments, 1),
+        fToBind = this,
+        fNOP    = function() {},
+        fBound  = function() {
+          return fToBind.apply(this instanceof fNOP && oThis
+                 ? this
+                 : oThis,
+                 aArgs.concat(Array.prototype.slice.call(arguments)));
+        };
+
+    fNOP.prototype = this.prototype;
+    fBound.prototype = new fNOP();
+
+    return fBound;
   };
 }
